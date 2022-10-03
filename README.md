9e8523520cabe65926cc30efd7f36c28b307462b
Antoine Montes <antoine.montes@pickyourskills.com>
2022-10-03 23:24:14 +0200

    add readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# tJSONAPI
+
+tJSONAPI is a fully typesafe adapter.
+
+It allows you to convert a raw JSONAPI response to a simpler and usable Javascript object.
+
--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,19 @@
-import { Attribute } from "./types/attribute";
-import { ApiClient } from "./client";
 import { JsonApiAdapter } from "./json-api-adapter";
-import { EntityFactory } from "./types/entity";
-import { Relationship } from "./types/relationship";
+import { Adapter } from "./adapter";
+import {
+  Attribute,
+  Entity,
+  EntityFactory,
+  Relationship,
+  RelationshipTypeEnum,
+} from "./types";
 
-class UserProject extends EntityFactory() {
-  @Attribute.required()
-  public id!: string;
-
-  @Attribute.required("project_id")
-  public projectId!: string;
-
-  @Attribute.optional("user_id")
-  public userId?: string;
-
-  @Attribute.required()
-  public simulated!: boolean;
-}
-
-class ProjectStatus extends EntityFactory() {
-  @Attribute.required()
-  public id!: string;
-
-  @Attribute.required()
-  public name!: string;
-}
-
-class Project extends EntityFactory() {
-  @Attribute.required()
-  public id!: string;
-
-  @Attribute.required()
-  public name!: string;
-
-  @Attribute.optional()
-  public description?: string;
-
-  @Relationship.toMany(UserProject, "user_projects")
-  public userProjects?: UserProject[];
-
-  @Relationship.toOne(ProjectStatus, "projectstatus")
-  public projectStatus?: ProjectStatus;
-
-  public describe(): string {
-    return this.id + ": " + this.name;
-  }
-}
-
-const apiClient = new ApiClient(
-  "http://localhost:5000/api/v1/",
-  new JsonApiAdapter()
-);
-
-apiClient
-  .getMany("project", Project, [UserProject, ProjectStatus])
-  .then((projects) => console.log(projects));
+export {
+  JsonApiAdapter,
+  Adapter,
+  Attribute,
+  Entity,
+  EntityFactory,
+  Relationship,
+  RelationshipTypeEnum,
+};
